f5f260349b69ea84088486dd1fbea0c2787f14d6
Prathviraj1 <84025534+Prathviraj1@users.noreply.github.com>
2022-01-22 23:14:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 import csv
 import pandas as pd
-
-# file = open("AIML Dataset.csv")
-# print(type(file))
-# csvreader = csv.reader(file)
-# header = []
-# header = next(csvreader)
-# print(header)
-#
-#
-# rows = []
-# for row in csvreader:
-#     rows.append(row)
-# print(rows)
-# file.close()
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 data= pd.read_csv("AIML Dataset.csv")
 print(data)
 print(data.columns)
 print(data.info())
 
-# if data.columns[-2] or data.columns[-1] == 1:
-#     print("Transaction is Fraud")
+
 print(data['isFraud'].value_counts())
 legit = data[data.isFraud == 0]
 fraud = data[data.isFraud == 1]
+flaggedfraud = data[data.isFlaggedFraud == 1]
 print(legit.shape)
 print(fraud.shape)
+print(flaggedfraud.shape)
 print(legit.describe())
 print(fraud.describe())
 
@@ -37,7 +27,29 @@ print(new_dataset.head())
 print(new_dataset.tail())
 print(new_dataset['isFraud' and "isFlaggedFraud"].value_counts())
 print(data.groupby('isFraud').mean())
-X = new_dataset.drop(columns='isFraud' and 'isFlaggedFraud', axis=1)
-Y = new_dataset['isFraud' and 'isFlaggedFraud']
+
+X = new_dataset.drop(new_dataset.iloc[:,9:11],axis=1)
+del X['type']
+del X['nameDest']
+del X['nameOrig']
+Y = new_dataset['isFraud']
+
+print("the new data set after dropping the columns is ")
+print()
 print(X)
 print(Y)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+print(X.shape, X_train.shape, X_test.shape)
+
+model = LogisticRegression()
+
+model.fit(X_train, Y_train)
+
+
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy on Training data : ', training_data_accuracy)
+
