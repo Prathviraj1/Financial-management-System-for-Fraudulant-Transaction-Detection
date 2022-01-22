f60e2bd11289582179cf06189ba8621b5761f24f
Prathviraj1 <84025534+Prathviraj1@users.noreply.github.com>
2022-01-22 19:04:20 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -20,8 +20,24 @@ print(data)
 print(data.columns)
 print(data.info())
 
+# if data.columns[-2] or data.columns[-1] == 1:
+#     print("Transaction is Fraud")
 print(data['isFraud'].value_counts())
 legit = data[data.isFraud == 0]
 fraud = data[data.isFraud == 1]
 print(legit.shape)
 print(fraud.shape)
+print(legit.describe())
+print(fraud.describe())
+
+
+legit_sample = legit.sample(n=8213)
+new_dataset = pd.concat([legit_sample, fraud], axis=0)
+print(new_dataset.head())
+print(new_dataset.tail())
+print(new_dataset['isFraud' and "isFlaggedFraud"].value_counts())
+print(data.groupby('isFraud').mean())
+X = new_dataset.drop(columns='isFraud' and 'isFlaggedFraud', axis=1)
+Y = new_dataset['isFraud' and 'isFlaggedFraud']
+print(X)
+print(Y)
